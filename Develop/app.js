639ed2393b8f8dc54cb0bfccbbbd633b2cafec99
Matthew <mcraig150@gmail.com>
2020-07-24 00:54:57 -0400

    Added code to employee.js

--- Develop/app.js
+++ Develop/app.js
@@ -11,25 +11,54 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const getInfo = [
+    {
+        name: "title",
+        message: "What is your role",
+        choices: ["Manager", "Engineer", "Intern"],
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    },
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    {
+        name: "firstName",
+        message: "Whats your first name?",
+    },
+    {
+        name: "lastName",
+        message: "Whats your last name?",
+    },
+    {
+        name: "email",
+        message: "Whats your email address?",
+    },
+    {
+        name: "workNumber",
+        message: "Whats your work phone number?",
+    },
+    {
+        name: "schoolName",
+        message: "What school did you attend?",
+    },
+    {
+        name: "gitHub",
+        message: "Whats your GitHub account ",
+    },
+];
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    async function start(){
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    //calls getInfo to prompt the user with questions
+    const info = await inquirer.prompt(getInfo);
+
+    //assign variables based on users input
+    const title = info.title;
+    const firstName = info.firstName;
+    const lastName = info.lastName;
+    const email = info.email;
+    const phone = info.workNumber;
+    const school = info.schoolName;
+    const git = info.gitHub;
+
+}
+start();
+
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,30 @@
 // TODO: Write code to define and export the Employee class
+ class Employee {
+
+    constructor (employeeId, firstName, lastName, title, email, officeNumber) {
+        this.employeeId = id;
+		this.firstName = fName;
+		this.lastName = lName;
+		this.title = title;
+		this.email = email;
+		this.officeNumber = officeNum;
+    }
+
+    getOffice() {
+        return this.officeNum;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+ }
+module.exports = Employee;
