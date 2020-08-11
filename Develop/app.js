c667054e1eac66b61ebce2dacf9e05ab070ab29b
Matthew <mcraig150@gmail.com>
2020-08-11 01:44:58 -0400

    Finished logic and added render call

--- Develop/app.js
+++ Develop/app.js
@@ -10,22 +10,24 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMembers = [];
 
+const check = [
+    {
+        name: "check",
+        message: "do you want to add another member"
+    }
+]
 const getInfo = [
     {
-        name: "title",
+        name: "role",
         message: "What is your role",
         choices: ["Manager", "Engineer", "Intern"],
 
     },
-
     {
-        name: "firstName",
-        message: "Whats your first name?",
-    },
-    {
-        name: "lastName",
-        message: "Whats your last name?",
+        name: "name",
+        message: "Whats your name?"
     },
     {
         name: "email",
@@ -33,32 +35,103 @@ const getInfo = [
     },
     {
         name: "workNumber",
-        message: "Whats your work phone number?",
-    },
-    {
-        name: "schoolName",
-        message: "What school did you attend?",
-    },
-    {
-        name: "gitHub",
-        message: "Whats your GitHub account ",
-    },
+        message: "Whats your work ID number?",
+    }
 ];
 
-    async function start(){
+const school = [
+    {
+        name: "school",
+        message: "What school are you attending"
+    }
+];
+
+const gitHub = [
+    {
+        name: "git",
+        message: "Whats your gitHub username"
+    }
+];
+
+const office = [
+    {
+        name: "office",
+        message: "Whats your office number"
+    }
+];
+
+    async function createTeam(){
 
     //calls getInfo to prompt the user with questions
     const info = await inquirer.prompt(getInfo);
 
     //assign variables based on users input
-    const title = info.title;
-    const firstName = info.firstName;
-    const lastName = info.lastName;
+    const role = info.role;
+    const name = info.name;
     const email = info.email;
-    const phone = info.workNumber;
-    const school = info.schoolName;
-    const git = info.gitHub;
+    const ID = info.workNumber;
 
-}
-start();
+    if(role === "Intern"){
+        const internSchool = await inquirer.prompt(school);
+        const schoolName = internSchool.school;
+
+        let intern = new Intern(name, ID, email, schoolName);
+        teamMembers.push(intern);
+        
+
+        let newMem = await inquirer.prompt(check);
+        let ans = newMem.check;
+        if(ans === "yes") {
+            createTeam();
+        }
+        else {
+            buildTeam();
+        }
+    }
+
+    if(role === "Engineer") {
+        const git = await inquirer.prompt(gitHub);
+        const gitName = git.git;
+
+        let eng = new Engineer(name, ID, email, gitName);
+        teamMembers.push(eng);
+        
+
+        let newMem = await inquirer.prompt(check);
+        let ans = newMem.check;
+        if(ans === "yes") {
+            createTeam();
+        }
+        else {
+            buildTeam();
+        }
+    }
+
+    if(role === "Manager") {
+        const officeNum = await inquirer.prompt(office);
+        const officeNumber = officeNum.office;
+
+        let man = new Manager(name, ID, email, officeNumber);
+        teamMembers.push(man);
+
+        let newMem = await inquirer.prompt(check);
+        let ans = newMem.check;
+        if(ans === "yes") {
+            createTeam();
+        }
+        else {
+            buildTeam();
+        }
+    }
+
+
+    
+    function buildTeam() {
+        if(!fs.existsSync(OUTPUT_DIR)) {
+          fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath,render(teamMembers),"utf-8");
+      }
+    
+}createTeam();
 
